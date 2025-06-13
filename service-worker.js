660cf144067b3ca57caf0993226ce6949a1f1729
NetworkEC <netwkec@gmail.com>
2025-06-13 04:01:06 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'agora-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
